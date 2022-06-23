b25525680f038f13fa5e50d01bc7284ad942779e
l463464 <106147032+l463464@users.noreply.github.com>
2022-06-23 15:31:18 +0200

    piligrimmm
    test

---
+++ main.js
@@ -0,0 +1,22 @@
+const cards = document.querySelectorAll('.card');
+
+for (let i = 0; i < cards.length; i++) {
+    const card = cards[i];
+    card.addEventListener('mousemove', startrotate);
+    card.addEventListener('mouseout', stoprotate);
+}
+
+function startrotate(event) {
+     const cardItem = this.querySelector('.card-item');
+     const halfHeight = cardItem.offsetHeight / 2;
+     const halfWidth = cardItem.offsetWidth / 2;
+     cardItem.style.transform = 'rotateX('+ - (event.offsetY - halfHeight) / 20 +'deg) rotateY('+ (event.offsetX - halfWidth) / 20 + 'deg)'
+
+}
+
+function stoprotate(event) {
+    const cardItem = this.querySelector('.card-item');
+    cardItem.style.transform = 'rotateX(0)';
+
+}
+ 
